b9e2aadddf6a45f78b205fd728476fe199f61121
janrembold <janrembold@gmx.net>
2018-10-20 08:52:37 +0200

    extract keys as consts

--- src/staffing/projectStatus.js
+++ src/staffing/projectStatus.js
@@ -1,15 +1,23 @@
+export const NEW = 'NEW';
+export const SEARCH = 'SEARCH';
+export const WAITFEEDBACK = 'WAITFEEDBACK';
+export const HOLD = 'HOLD';
+export const BLOCKER = 'BLOCKER';
+export const ACTIVE = 'ACTIVE';
+
 export const PROJECT_STATUS = {
-  'NEW': 'New Project',
-  'SEARCH': 'Search for resources',
-  'WAITFEEDBACK': 'Waiting for feedback',
-  'HOLD': 'On hold',
-  'BLOCKER': 'Blocker',
-  'ACTIVE': 'Active Development'
+  NEW: 'New Project',
+  SEARCH: 'Search for resources',
+  WAITFEEDBACK: 'Waiting for feedback',
+  HOLD: 'On hold',
+  BLOCKER: 'Blocker',
+  ACTIVE: 'Active Development'
 };
 
 export const getProjectStatusByKey = (key) => {
-  if(!PROJECT_STATUS[key]) {
+  const uppercaseKey = key.toUpperCase();
+  if(!PROJECT_STATUS[uppercaseKey]) {
     return `Unknown project status translation for key = "${key}"`;
   }
-  return PROJECT_STATUS[key];
+  return PROJECT_STATUS[uppercaseKey];
 };
